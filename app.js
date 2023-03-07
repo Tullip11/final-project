4c410ae7708eb098ff48b2de1b49ae7ae1a0335c
Tullip11 <120431190+Tullip11@users.noreply.github.com>
2023-03-06 18:18:12 -0600

    update

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("you are smarter than you give yourself credit for")
+const menuOneButton = document.getElementById("menu-button-1")
+
+menuOneButton.addEventListener("click", function () {
+  console.log("are you working?")
+  document.getElementById("menu-1").classList.toggle("show")
+})
+
+
+const menuTwoButton = document.getElementById("menu-button-2")
+
+menuTwoButton.addEventListener("click", function () {
+  console.log("yes")
+  document.getElementById("menu-2").classList.toggle("show")
+})
+
+
+const menuThreeButton = document.getElementById("menu-button-3")
+
+menuThreeButton.addEventListener("click", function () {
+  console.log("i do")
+  document.getElementById("menu-3").classList.toggle("show")
+})
